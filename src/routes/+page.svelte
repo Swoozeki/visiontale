a74e3f3e029dc312a2d9185468a0750604b2dc3e
Sujit Karki <swoozeki>
2023-09-08 18:58:40 -0400

    added tailwind and daisy UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-6">
+	<h1 class="text-2xl">Hello world!</h1>
+	<a href="/about" class="btn">About</a>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<div class="p-6">
+	<h1 class="text-2xl">About page</h1>
+</div>
